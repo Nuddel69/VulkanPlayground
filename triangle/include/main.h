5eee664a2f419f9e00792e89dc9a41f7fbe199ef
Mats Bjønnes <mats.bjonnes@gmail.com>
2026-06-06 14:24:25 +0200

    triangle: Add validation layers and support for manual extensions

--- triangle/include/main.h
+++ triangle/include/main.h
@@ -1,7 +1,9 @@
-#include <GLFW/glfw3.h>
-#include <vulkan/vulkan_core.h>
 #ifndef INCLUDEincludemainmainh_
 #define INCLUDEincludemainmainh_
+
+#include <GLFW/glfw3.h>
+#include <stdint.h>
+#include <vulkan/vulkan_core.h>
 
 #define WINDOW_HEIGHT 600
 #define WINDOW_WIDTH 800
@@ -9,7 +11,9 @@
 struct vulkan_cfg {
   VkInstance *_inst;
   GLFWwindow *_window;
+
   const char **validationLayers;
+  uint32_t validationLayers_n;
 };
 
 #endif // INCLUDEincludemainmainh_
--- triangle/main.c
+++ triangle/main.c
@@ -44,6 +44,35 @@ static int initWindow(struct vulkan_cfg *cfg) {
   return 0;
 }
 
+static const char **getRequiredInstanceExtensions(struct vulkan_cfg *cfg,
+                                                  uint32_t *count) {
+  uint32_t extensions_n = 0;
+  const char **extensions_buf;
+
+  extensions_buf = glfwGetRequiredInstanceExtensions(&extensions_n);
+
+  if (!extensions_n) {
+    fprintf(stderr, "Error fetching required extensions\n");
+    return NULL; // TODO: Better errno
+  }
+
+  const char **extensions =
+      (const char **)malloc(extensions_n * (sizeof(char *)));
+
+  for (size_t i = 0; i < extensions_n; i++) {
+    extensions[i] = extensions_buf[i];
+  }
+
+  if (enableValidationLayers) {
+    extensions_n++;
+    extensions = realloc(extensions, extensions_n * sizeof(char *));
+    extensions[extensions_n - 1] = VK_EXT_DEBUG_UTILS_EXTENSION_NAME;
+  }
+
+  *count = extensions_n;
+  return extensions;
+}
+
 static int createVulkanInstance(struct vulkan_cfg *cfg) {
   int status;
 
@@ -56,41 +85,36 @@ static int createVulkanInstance(struct vulkan_cfg *cfg) {
       VK_MAKE_VERSION(0, 1, 0),
       VK_API_VERSION_1_4};
 
-  uint32_t glfwExtensionCount = 0;
-  uint32_t vkSupportedExtensionCount = 0;
-  ;
+  uint32_t requiredExtensions_n = 0;
+  uint32_t supportedExtensions_n = 0;
 
-  const char **glfwExtensions =
-      glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-
-  if (!glfwExtensionCount) {
-    fprintf(stderr, "Error fetching required extensions\n");
-    return -1; // TODO: Better errno
-  }
+  const char **requiredExtensions =
+      getRequiredInstanceExtensions(cfg, &requiredExtensions_n);
 
   // It seems a little strange needing to repeat this, but as far as I can
-  // tell by the docs, you can _either_ get the number of extensions, _or_ get
-  // their names as the property-pointer needs to be large enough to store them
-  // all.
-  vkEnumerateInstanceExtensionProperties(NULL, &vkSupportedExtensionCount,
-                                         NULL);
-  VkExtensionProperties properties[vkSupportedExtensionCount];
-  status = vkEnumerateInstanceExtensionProperties(
-      NULL, &vkSupportedExtensionCount, properties);
+  // tell by the docs, you can _either_ get the number of extensions, _or_
+  // get their names as the property-pointer needs to be large enough to
+  // store them all.
+  vkEnumerateInstanceExtensionProperties(NULL, &supportedExtensions_n, NULL);
+  VkExtensionProperties supportedExtensions[supportedExtensions_n];
+  status = vkEnumerateInstanceExtensionProperties(NULL, &supportedExtensions_n,
+                                                  supportedExtensions);
   if (status != VK_SUCCESS) {
     fprintf(stderr, "Failed to fetch vulkan supported extensions (error %d)\n",
             status);
     return status;
   }
 
-  printf("Supported Extension Count : (%d)\n", vkSupportedExtensionCount);
+  printf("Supported Extension Count: (%d)\n", supportedExtensions_n);
+  printf("Required Extension Count: (%d)\n", requiredExtensions_n);
 
-  for (size_t i = 0, match = 0; i < glfwExtensionCount; i++) {
+  for (size_t i = 0, match = 0; i < requiredExtensions_n; i++) {
     match = 0;
-    const char *extension = glfwExtensions[i];
+    const char *extension = requiredExtensions[i];
+
     printf("Extension (%zu): %s\n", i + 1, extension);
-    for (size_t j = 0; j < vkSupportedExtensionCount; j++) {
-      const char *supported = properties[j].extensionName;
+    for (size_t j = 0; j < supportedExtensions_n; j++) {
+      const char *supported = supportedExtensions[j].extensionName;
 
       if (!strcmp(extension, supported)) {
         match = 1;
@@ -104,15 +128,51 @@ static int createVulkanInstance(struct vulkan_cfg *cfg) {
     }
   }
 
+  //* Verify validation layers
+
+  uint32_t supportedLayer_n = 0;
+
+  vkEnumerateInstanceLayerProperties(&supportedLayer_n, NULL);
+  VkLayerProperties supportedLayer[supportedLayer_n];
+  status =
+      vkEnumerateInstanceLayerProperties(&supportedLayer_n, supportedLayer);
+  if (status != VK_SUCCESS) {
+    fprintf(stderr,
+            "failed to fetch vulkan supported validation layers (error %d)\n",
+            status);
+    return status;
+  }
+
+  printf("Supported layers: %d\n", supportedLayer_n);
+  for (size_t i = 0, match = 0;
+       ((i < cfg->validationLayers_n) && (enableValidationLayers)); i++) {
+    match = 0;
+    const char *layer = cfg->validationLayers[i];
+    printf("Layer (%zu): %s\n", i + 1, layer);
+    for (size_t j = 0; j < supportedLayer_n; j++) {
+      const char *supported = supportedLayer[j].layerName;
+
+      if (!strcmp(layer, supported)) {
+        match = 1;
+        break;
+      }
+    }
+
+    if (!match) {
+      fprintf(stderr, "Required validation layer not supported: %s\n", layer);
+      return -1; // TODO: Better errno
+    }
+  }
+
   const struct VkInstanceCreateInfo _instance_info = {
       VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
       NULL,
       0,
       &_app_info,
-      0,
-      NULL,
-      glfwExtensionCount,
-      glfwExtensions};
+      (enableValidationLayers ? cfg->validationLayers_n : 0),
+      cfg->validationLayers,
+      requiredExtensions_n,
+      requiredExtensions};
 
   status = vkCreateInstance(&_instance_info, NULL, cfg->_inst);
 
@@ -164,11 +224,7 @@ int main(int argc, char *argv[]) {
   int status;
   VkInstance inst;
 
-  struct vulkan_cfg cfg = {&inst, .validationLayers = validationLayers};
-
-#ifdef RELEASE_BUILD
-  printf("Release build!\n");
-#endif
+  struct vulkan_cfg cfg = {&inst, .validationLayers = validationLayers, 1};
 
   status = run(&cfg);
   if (status) {
